d7cbb6df1aeb4b1f28f59f3d5672e75fd0b56020
decidee <e22817@protonmail.com>
2022-11-15 15:59:28 -0500

    Removed unnecessary import

--- Habitual/Routes/FrontGate.py
+++ Habitual/Routes/FrontGate.py
@@ -1,6 +1,5 @@
 import os
 import time
-from plistlib import UID
 from dotenv import load_dotenv
 from flask import render_template, redirect, url_for, request, Blueprint, session, flash
 from flask_login import login_user,current_user,logout_user, login_required
